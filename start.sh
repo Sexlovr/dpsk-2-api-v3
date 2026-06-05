85a1c3d3339c1d6e6b412a84b23bff96641e6c0d
ahemmm <akifm1373@gmail.com>
2026-06-06 02:18:52 +0600

    Update VNC setup to use websockify
    Replaced noVNC proxy with websockify for WebSocket bridge.

--- start.sh
+++ start.sh
@@ -11,8 +11,9 @@ fluxbox &
 # Start VNC Server internally on port 5900
 x11vnc -display :99 -forever -shared -bg -nopw -rfbport 5900
 
-# Start noVNC (websocket wrapper) on port 6080
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
+# Start websockify (noVNC WebSocket bridge) on port 6080
+# Serves the noVNC web client AND proxies WebSocket to VNC
+websockify --web /usr/share/novnc 6080 localhost:5900 &
 
 # Start our Node Express app (Hugging Face routes to 7860 natively)
 export PORT=7860
