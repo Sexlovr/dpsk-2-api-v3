0dcaf479af380d69f7bcaae61b12b7d126d426a0
ahemmm <akifm1373@gmail.com>
2026-06-06 06:56:07 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# Setup virtual display
-export DISPLAY=:99
-Xvfb :99 -screen 0 1280x720x24 &
-sleep 2
-
-# Start a basic window manager
-fluxbox &
-
-# Start VNC Server internally on port 5900
-x11vnc -display :99 -forever -shared -bg -nopw -rfbport 5900
-
-# Start websockify (noVNC WebSocket bridge) on port 6080
-# Serves the noVNC web client AND proxies WebSocket to VNC
-websockify --web /usr/share/novnc 6080 localhost:5900 &
-
-# Start our Node Express app (Hugging Face routes to 7860 natively)
+# Start our Node Express app ONLY - no VNC at boot
+# VNC will be launched on-demand by browser_controller.js
 export PORT=7860
 node index.js
