ee6548d1638b6da039ddb8acb71746c308eca64b
ahemmm <akifm1373@gmail.com>
2026-06-06 09:17:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ app.use('/websockify', createProxyMiddleware({
     }
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-api-key');
+    if (req.method === 'OPTIONS') return res.status(200).end();
+    next();
+});
+
 app.use(express.json({ limit: '10mb' }));
 
 var PORT = process.env.PORT || 3000;
